004f7fc87e055ec973a23f7140d443f3e3ca53ca
vlada-marchenko <vvvvadya44@gmail.com>
2026-02-19 01:42:03 +0100

    add fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ const protectedRoutes = ['/shop', '/medicine', '/statistics'];
 
 const authRoutes = ['/login', '/register'];
 
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   const shopId = request.cookies.get('shopId')?.value;
   const { pathname } = request.nextUrl;
