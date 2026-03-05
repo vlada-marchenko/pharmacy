843e3e62af092b321f68f772a1fa20a2000f5cc9
vlada-marchenko <vvvvadya44@gmail.com>
2026-03-05 03:45:01 +0100

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,15 +9,25 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   const shopId = request.cookies.get('shopId')?.value;
   const { pathname } = request.nextUrl;
+
+  if (pathname.includes('/undefined')) {
+    if (shopId && shopId !== 'undefined') {
+      const newPath = pathname.replace('undefined', shopId);
+      return NextResponse.redirect(new URL(newPath, request.url));
+    }
+    return NextResponse.redirect(new URL('/shop/create', request.url));
+  }
+
   const isProtectedRoute = protectedRoutes.some((route) =>
     pathname.startsWith(route),
   );
+
   if (isProtectedRoute && !token) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
   if (authRoutes.includes(pathname) && token) {
-    if (shopId) {
+    if (shopId && shopId !== 'undefined') {
     return NextResponse.redirect(new URL(`/shop/${shopId}`, request.url));
   } else {
     return NextResponse.redirect(new URL('/shop/create', request.url));
